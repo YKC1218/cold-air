8717bd744c4c8e38222a1fc40282f091293dd392
Jimmy kc <yimmychung@gmail.com>
2020-10-23 01:36:28 +0800

    first commit

---
+++ build/precache-manifest.ed8874be41c911474a4a784388c2c291.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b38c63f94eacbc94126f669615b8e4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3db353acb1826e55323f",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "21ea98b88b5041a91b02",
+    "url": "/static/css/main.64bcd7a0.chunk.css"
+  },
+  {
+    "revision": "3db353acb1826e55323f",
+    "url": "/static/js/2.7f67bf7d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7f67bf7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21ea98b88b5041a91b02",
+    "url": "/static/js/main.4befecdc.chunk.js"
+  },
+  {
+    "revision": "42633c7c1c0bc3a5052c",
+    "url": "/static/js/runtime-main.16ecf91d.js"
+  }
+]);
